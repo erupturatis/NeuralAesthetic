ad2349993fd94b320c44822918f8ad8d7923ac3d
erupturatis <bb.octav@gmail.com>
2023-02-19 21:25:20 +0200

    working tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
   testEnvironment: "node",
-  testPathIgnorePatterns: ["./__tests__/index.js"],
+  testPathIgnorePatterns: [
+    "./__tests__/index.js",
+    "./__tests__/react_integration",
+  ],
 };
