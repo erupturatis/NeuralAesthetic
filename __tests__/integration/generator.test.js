cd1ac8122a5eb7b3906e841523ad66a6183484b6
erupturatis <bb.octav@gmail.com>
2023-02-20 20:41:49 +0200

    testing server testing

--- __tests__/integration/generator.test.js
+++ __tests__/integration/generator.test.js
@@ -4,6 +4,7 @@ import puppeteer from "puppeteer";
 describe("Tests Base painter functionality in the context of a react app", () => {
   let browser;
   let page;
+
   beforeAll(async () => {
     // launch the browser and create a new page
     browser = await puppeteer.launch();
@@ -16,9 +17,8 @@ describe("Tests Base painter functionality in the context of a react app", () =>
   });
 
   test("testing app starting", async () => {
-    await page.goto("http://localhost:5173"); // vite default port
+    await page.goto("http://localhost:5173");
     const svg = await page.$("#root-svg");
-    console.log(svg);
     const mySvg = new BasePainter(svg);
   });
 });
